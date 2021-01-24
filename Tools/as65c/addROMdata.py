a17eb3baac035f6106a9c9c4a9c9c18f046f1cf4
MrL314 <11429905+MrL314@users.noreply.github.com>
2021-01-23 19:31:51 -0500

    v0.6
    fixed display issues, now shows warnings when data is going to be overwritten

--- Tools/as65c/addROMdata.py
+++ Tools/as65c/addROMdata.py
@@ -24,15 +24,13 @@ DATA = []
 written_ranges = []
 
 
-def write_data(file, s_off, d_off, d_size):
+def write_data(file, s_off, d_off, d_size, data):
 	global DATA
 
-	data = []
 
-	print("Writing 0x" + format(d_size, "04x") + " bytes from file " + str(file) + " from offset $" + format(s_off, "06x") + " to offset $" + format(d_off, "06x"))
+	print("Writing 0x" + format(d_size, "04x") + " bytes from offset $" + format(s_off, "06x") + " in file " + str(file) + " to offset $" + format(d_off, "06x"))
 
-	with open(file, "rb") as FILE:
-		data = FILE.read()
+	
 
 		#print(format(len(data), "04x"))
 
@@ -51,8 +49,8 @@ def write_data(file, s_off, d_off, d_size):
 def overwrite_warning(file, start, end, o_start, o_end):
 	d_size = (end - start) + 1
 
-	print("[WARNING] Overwriting 0x" + format(size, "06x") + " bytes in file " + str(file) + " from offset $" + format(start, "06x") + " to offset $" + format(end, "06x"))
-	print("          Overwrites data from this inserted block: [$" + format(o_start, "06x") + ", $" + format(o_end, "06x") + "]")
+	print("[WARNING] Overwriting 0x" + format(d_size, "04x") + " bytes in file " + str(file) + " from $" + format(start, "06x") + " to $" + format(end, "06x"))
+	print("          Overwrites data from this inserted block: [$" + format(o_start, "06x") + ", $" + format(o_end, "06x") + "]\n")
 
 
 
@@ -117,34 +115,44 @@ with open(optionsfile, "r") as R:
 			d_off = int("0x" + dat[2].lstrip().rstrip().replace("0x", ""), 16)
 			size  = int("0x" + dat[3].lstrip().rstrip().replace("0x", ""), 16)
 
-			write_data(file, s_off, d_off, size)
+			data = []
+
+			with open(file, "rb") as FILE:
+				data = FILE.read()
+
+			if s_off + size > len(data):
+				size = len(data) - s_off
+
+
+			write_data(file, s_off, d_off, size, data)
 
 			start = d_off
 			end = d_off+size-1
 
 			for o_file, o_start, o_end in written_ranges:
 
-				if file != o_file:
-					continue	# not same file, do not worry about overwrite
-
 				r_start = min(start, o_start)
 				r_end = max(end, o_end)
 
 				if r_start == start and r_end == end:		# overwriting complete block
-					overwrite_warning(file, o_start, o_end, o_start, o_end)
-					
+					overwrite_warning(o_file, o_start, o_end, o_start, o_end)
+					break
+
 				elif r_start == o_start and r_end == o_end:	# overwriting inside of block
-					overwrite_warning(file, start, end, o_start, o_end)
+					overwrite_warning(o_file, start, end, o_start, o_end)
+					break
 
 				elif r_start == start and r_end == o_end:	# new block possibly overwrites left side
 					if min(end, o_start) == o_start:
-						overwrite_warning(file, o_start, end, o_start, o_end)	# overwrites part of left side of block
+						overwrite_warning(o_file, o_start, end, o_start, o_end)	# overwrites part of left side of block
+						break
 					elif min(end, o_start) == end:
 						continue		# new is completely to the left of block, no overwrite 
 
 				elif r_start == o_start and r_end == end:	# new block possibly overwrites right side
 					if min(o_end, start) == start:
-						overwrite_warning(file, start, o_end, o_start, o_end)	# overwrites part of right side of block
+						overwrite_warning(o_file, start, o_end, o_start, o_end)	# overwrites part of right side of block
+						break
 					elif min(o_end, start) == o_end:
 						continue		# new is completely to the right of block, no overwrite 
 				
